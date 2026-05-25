5db7fc49f8e6ebcc7cc94922e4e94d3902aba965
Gillian Tunney <gbtunney@mac.com>
2026-05-25 00:58:06 -0400

    feat(root): test

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,4 @@
-import {
-    EsLint,
-    expandExtensions,
-    TS_FILE_EXTENSIONS,
-} from '@snailicid3/config'
+import { EsLint } from '@snailicid3/config'
 
 import url from 'node:url'
 
@@ -10,31 +6,4 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 const CONFIG = EsLint.config(__dirname)
 
-export default EsLint.defineConfig([
-    ...CONFIG,
-
-    {
-        files: expandExtensions(TS_FILE_EXTENSIONS, '**/src/**/*'),
-
-        name: 'Naming: allow ids for paramaters',
-
-        rules: {
-            '@typescript-eslint/naming-convention': [
-                'error',
-
-                {
-                    custom: {
-                        match: true,
-
-                        regex: '^([a-zA-Z][a-zA-Z0-9_]{2,}|id|db|fs|ctx|req|res)$',
-                    },
-
-                    format: ['camelCase'],
-
-                    selector: 'parameter',
-                },
-            ],
-        },
-    },
-
-])
+export default EsLint.defineConfig([...CONFIG])
